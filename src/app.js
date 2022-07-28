c3d3c6fe069bda7c721fad659e7ba7d6addcb472
Jon Eastin <picardncce@gmail.com>
2022-07-28 13:17:41 -0700

    bringing the backend to a working state before continuning

--- src/app.js
+++ src/app.js
@@ -3,14 +3,15 @@ const cors = require('cors')
 
 const errorHandler = require('./errors/errorHandler')
 const notFound = require('./errors/notFound')
-const observationRouter = require('./observations/observations.router')
+// const observationRouter = require('./observations/observations.router')
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-/* repalcing this block
+// repalcing this block
+
 let nextId = 1
 app.post('/observations', (req, res) => {
 	const newObservation = req.body.data
@@ -21,9 +22,8 @@ app.post('/observations', (req, res) => {
 		data: newObservation,
 	})
 })
-*/
 
-app.use('/observations', observationRouter)
+// app.use('/observations', observationRouter)
 app.use(notFound)
 app.use(errorHandler)
 
