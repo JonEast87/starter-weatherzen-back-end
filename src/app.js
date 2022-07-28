7da57ec69583b935814228100e13ee9658641b2e
Jon Eastin <picardncce@gmail.com>
2022-07-28 12:58:31 -0700

    setting up backend to communicate with the new ui featuers added

--- src/app.js
+++ src/app.js
@@ -1,16 +1,30 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-const errorHandler = require("./errors/errorHandler");
-const notFound = require("./errors/notFound");
+const errorHandler = require('./errors/errorHandler')
+const notFound = require('./errors/notFound')
+const observationRouter = require('./observations/observations.router')
 
-const app = express();
+const app = express()
 
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 
+/* repalcing this block
+let nextId = 1
+app.post('/observations', (req, res) => {
+	const newObservation = req.body.data
 
-app.use(notFound);
-app.use(errorHandler);
+	newObservation.observation_id = nextId++
 
-module.exports = app;
+	res.status(201).json({
+		data: newObservation,
+	})
+})
+*/
+
+app.use('/observations', observationRouter)
+app.use(notFound)
+app.use(errorHandler)
+
+module.exports = app
